ffc7360b683d0ae03947068af6ebe1059e4cd7e1
RakanEngineer <rakan.engineer@gmail.com>
2025-11-12 10:17:01 +0100

    Added Person and its derived classes

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BasicOOP
+{
+    internal class Person
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+    }
+    internal class Admin : Employee
+    {
+        public string Department { get; set; } = string.Empty;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
-﻿namespace BasicOOP
+﻿
+using System;
+
+namespace BasicOOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Part1();
+        }
+
+        private static void Part1()
+        {
+            Person p = new Person();
+            p.Name = "Kalle";
+            //p.Salary
+            Employee e = new Employee();
+            e.Name = "Anna";
+            e.Salary = 25000;
+            Admin a = new Admin();
+            a.Name = "Kim";
+            a.Salary = 30000;
+            a.Department = "IT";
+
+            Employee admin = new Admin();
+            Person admin2 = new Admin();
+            Admin admin3 = new Admin();
+
+            Do(p);
+            Do(e);
+            Do(a);
+            Do(admin);
+            Do(admin2);
+            Do(admin3);
+
+            List<Person> people = new List<Person>
+            {
+                p,
+                e,
+                a,
+                admin,
+                admin2,
+                admin3
+            };
+
+            Console.ReadKey();
+        }
+
+        private static void Do(Person person)
+        {
+            Console.WriteLine(person);
         }
     }
 }
